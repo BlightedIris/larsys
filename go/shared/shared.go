122477a02e91174120343da82986b02eda17a750
BlightedIris <blightediris@gmail.com>
2026-07-21 20:21:53 +0100

    Added more features to shared file

--- go/shared/shared.go
+++ go/shared/shared.go
@@ -1,5 +1,34 @@
 package shared
 
+import (
+	"os"
+	"os/user"
+	"path/filepath"
+)
+
+var PLUGIN_DIR = "/opt/larsys/plugins"
+var TOKEN_DIR = "/etc/larsys/tokens"
+var TOKEN_DIR_CL = "/etc/larsys/tokens/clients"
+var TOKEN_DIR_HS = "/etc/larsys/tokens/hosts"
+var LOG_DIR = "/var/log/larsys/"
+var STATE_DIR = "/var/lib/larsys/"
+
+func InitDirs() {
+	dirs := []string{
+		PLUGIN_DIR,
+		TOKEN_DIR_CL,
+		TOKEN_DIR_HS,
+		LOG_DIR,
+		STATE_DIR,
+	}
+
+	for _, dir := range dirs {
+		if err := os.MkdirAll(dir, 0o755); err != nil {
+			panic(err)
+		}
+	}
+}
+
 type LoggingConfig struct {
 	PATH  string
 	RULES int
@@ -8,7 +37,47 @@ type LoggingConfig struct {
 }
 
 type Node struct {
-	IP   string
-	PORT int
-	LOG  LoggingConfig
+	IP       string
+	PORT     int
+	LOG      LoggingConfig
+	USERNAME string
+	DEVICE   string
+}
+
+func GetUsername() string {
+	u, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	return u.Username
+}
+
+func GetTokenPath(name string, host bool) string {
+	var dest string
+	if host {
+		dest = "hosts"
+	} else {
+		dest = "clients"
+	}
+	return filepath.Join(TOKEN_DIR, dest, name, "token")
+}
+
+func ExtractParam(obj map[string]any, param string) any {
+	if obj == nil {
+		return ""
+	}
+
+	if value, exists := obj[param]; exists {
+		return value
+	}
+
+	return nil
+}
+
+func SaveToken(host, token string) error {
+	tokenDir := filepath.Join(TOKEN_DIR, "hosts", host)
+	if err := os.MkdirAll(tokenDir, 0o755); err != nil {
+		return err
+	}
+	return os.WriteFile(filepath.Join(tokenDir, "token"), []byte(token), 0o600)
 }
