f2dc9b6ddd009edef1938dff6c152ca350601871
BlightedIris <blightediris@gmail.com>
2026-07-21 20:21:39 +0100

    Added more features to the protocol

--- go/shared/proto.go
+++ go/shared/proto.go
@@ -1,60 +1,85 @@
 package shared
 
 type Action struct {
-	NAME string
+	NAME   string
 	PARAMS any
 }
 
+var PING = Action{
+	NAME:   "ping",
+	PARAMS: struct{}{},
+}
+
+type REGISTER_PARAMS struct {
+	NAME string
+}
+
 var REGISTER = Action{
-	NAME: "register",
-	PARAMS: struct{
-		CLIENT_NAME string
-	}{},
+	NAME:   "register",
+	PARAMS: REGISTER_PARAMS{},
+}
+
+type REVOKE_PARAMS struct {
+	NAME string
 }
 
 var REVOKE = Action{
-	NAME: "revoke",
-	PARAMS: struct{
-		CLIENT_NAME string
-	}{},
+	NAME:   "revoke",
+	PARAMS: REVOKE_PARAMS{},
+}
+
+type PLUGIN_INSTALL_PARAMS struct {
+	NAME string
 }
 
 var PLUGIN_INSTALL = Action{
-	NAME: "plugin/install",
-	PARAMS: struct{
-		NAME string
-	}{},
+	NAME:   "plugin/install",
+	PARAMS: PLUGIN_INSTALL_PARAMS{},
 }
 
-var PLUGIN_REMOVE = Action{
-	NAME: "plugin/remove",
-	PARAMS: struct{
-		NAME string
-	}{},
+type PLUGIN_UNINSTALL_PARAMS struct {
+	NAME string
+}
+
+var PLUGIN_UNINSTALL = Action{
+	NAME:   "plugin/uninstall",
+	PARAMS: PLUGIN_UNINSTALL_PARAMS{},
 }
 
 type Request struct {
-	SRC string
-	TOKEN string
+	SRC    string
+	TOKEN  string
 	ACTION Action
 }
 
 type Response struct {
+	SRC    string
 	STATUS int
-	MSG string
+	MSG    string
+	PARAMS any
 }
 
-var OK = Response {
-	STATUS : 0,
-	MSG : "OK",
+type REGISTERED_PARAMS struct {
+	TOKEN string
 }
 
-var UNAUTHORISED = Response {
-	STATUS : 1,
-	MSG : "Unauthorised",
+var REGISTERED = Response{
+	STATUS: 0,
+	MSG:    "+++ Client Registered +++",
+	PARAMS: REGISTERED_PARAMS{},
 }
 
-var ALREADY_REGISTERED = Response {
-	STATUS : 1,
-	MSG : "Client already registered",
+var OK = Response{
+	STATUS: 0,
+	MSG:    "OK",
+}
+
+var UNAUTHORISED = Response{
+	STATUS: 1,
+	MSG:    "Unauthorised",
+}
+
+var ALREADY_REGISTERED = Response{
+	STATUS: 1,
+	MSG:    "Client already registered",
 }
