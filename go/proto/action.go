bc839aec6ec3007bd0f3500be7e1df989535b8f9
BlightedIris <blightediris@gmail.com>
2026-07-22 01:53:03 +0100

    broken actions into their own file

---
+++ go/proto/action.go
@@ -0,0 +1,47 @@
+package proto
+
+type Action struct {
+	NAME   string
+	PARAMS any
+}
+
+var PING = Action{
+	NAME:   "ping",
+	PARAMS: struct{}{},
+}
+
+type REGISTER_PARAMS struct {
+	NAME string
+}
+
+var REGISTER = Action{
+	NAME:   "register",
+	PARAMS: REGISTER_PARAMS{},
+}
+
+type REVOKE_PARAMS struct {
+	NAME string
+}
+
+var REVOKE = Action{
+	NAME:   "revoke",
+	PARAMS: REVOKE_PARAMS{},
+}
+
+type PLUGIN_INSTALL_PARAMS struct {
+	NAME string
+}
+
+var PLUGIN_INSTALL = Action{
+	NAME:   "plugin/install",
+	PARAMS: PLUGIN_INSTALL_PARAMS{},
+}
+
+type PLUGIN_UNINSTALL_PARAMS struct {
+	NAME string
+}
+
+var PLUGIN_UNINSTALL = Action{
+	NAME:   "plugin/uninstall",
+	PARAMS: PLUGIN_UNINSTALL_PARAMS{},
+}
