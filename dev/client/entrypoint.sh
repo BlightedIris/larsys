9f1b95b0528e37ae4f5fce87dc6fba91a4b29303
BlightedIris <blightediris@gmail.com>
2026-07-22 15:36:51 +0100

    Added ping as test command

--- dev/client/entrypoint.sh
+++ dev/client/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-
+printf 'ping\n' | /usr/bin/larsys -host-ip=larsys-server -host-port=5454 -log=/var/log/larsys/daemon.log
 printf 'register\n' | /usr/bin/larsys -host-ip=larsys-server -host-port=5454 -log=/var/log/larsys/daemon.log
 printf 'register\n' | /usr/bin/larsys -host-ip=larsys-server -host-port=5454 -log=/var/log/larsys/daemon.log
 printf 'revoke\n' | /usr/bin/larsys -host-ip=larsys-server -host-port=5454 -log=/var/log/larsys/daemon.log
