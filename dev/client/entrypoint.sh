575db7e74baa0f610b15359a53e1a0e2ae89e55c
BlightedIris <blightediris@gmail.com>
2026-07-22 01:52:37 +0100

    added revoke step at the end of the entrypoint script as cleanup

--- dev/client/entrypoint.sh
+++ dev/client/entrypoint.sh
@@ -6,3 +6,4 @@ printf 'register\n' | /usr/bin/larsys -host-ip=larsys-server -host-port=5454 -lo
 printf 'revoke\n' | /usr/bin/larsys -host-ip=larsys-server -host-port=5454 -log=/var/log/larsys/daemon.log
 printf 'revoke\n' | /usr/bin/larsys -host-ip=larsys-server -host-port=5454 -log=/var/log/larsys/daemon.log
 printf 'register\n' | /usr/bin/larsys -host-ip=larsys-server -host-port=5454 -log=/var/log/larsys/daemon.log
+printf 'revoke\n' | /usr/bin/larsys -host-ip=larsys-server -host-port=5454 -log=/var/log/larsys/daemon.log
