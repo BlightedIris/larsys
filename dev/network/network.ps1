24f95436340c5fa21675902f7979655b28560d30
BlightedIris <blightediris@gmail.com>
2026-07-22 15:37:08 +0100

    Made network.ps1 more robust

--- dev/network/network.ps1
+++ dev/network/network.ps1
@@ -1,2 +1,15 @@
-docker network remove larsys-network
-docker network create larsys-network
+$SERVER="larsys-server"
+$CLIENT="larsys-client"
+$NETWORK="larsys-network"
+
+Write-Host "Stopping $SERVER"
+docker stop $SERVER
+
+Write-Host "Stopping $CLIENT"
+docker stop $CLIENT
+
+Write-Host "Stopping $NETWORK"
+docker network remove $NETWORK
+
+Write-Host "Starting $NETWORK"
+docker network create $NETWORK
